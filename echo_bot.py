fcab34cb3ba1e2a611398925c56c445cc5c2202c
Бондарев Алексей Александрович <a.bondarev@2gis.ru>
2017-11-21 16:53:15 +0300

    'sfd'

--- echo_bot.py
+++ echo_bot.py
@@ -6,8 +6,14 @@ bot = telebot.TeleBot("381499912:AAH73wXx4ZJQ75RL_ZgNFf2RVbs4NEWhGv8")
 def send_welcome(message):
 	bot.reply_to(message, "Hi, how are you doing?")
 
+@bot.message_handler(content_types=["sticker"])
+def send_welcome1(message):
+	chat_id = message.chat.id
+	bot.send_sticker(chat_id, message.sticker.file_id)
+
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
 	bot.reply_to(message, message.text)
+
 
 bot.polling()
